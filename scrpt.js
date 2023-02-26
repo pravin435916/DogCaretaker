d50457ec9c650b780e13964010ded25379ec7c0c
Pravin Nandankar <125211998+pravin435916@users.noreply.github.com>
2023-02-26 14:34:31 +0530

    first website

---
+++ scrpt.js
@@ -0,0 +1,10 @@
+function openform() {
+    document.getElementById("myform").style.display = "block";
+   
+}
+function  closeform() {
+    document.getElementById("myform").style.display = "none";
+}
+function menu() {
+    document.getElementsByClassName("nav").style.display = "block";
+}
